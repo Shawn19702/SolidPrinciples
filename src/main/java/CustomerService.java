712c66dc6e9c3efa5e449e87ab88ebb637a55bf7
Shawn19702 <shawnboodhoo5@gmail.com>
2022-10-16 23:15:01 -0400

    solid principles

---
+++ src/main/java/CustomerService.java
@@ -0,0 +1,9 @@
+public class CustomerService {
+
+    private final CustomerRepository customerDao;
+
+    public CustomerService(CustomerRepository customerDao) {
+        this.customerDao = customerDao;
+    }
+
+}
